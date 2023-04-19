8d196548f3ebc3f600f8e9ef4bbef8a643a55db1
riazonbin <riazonbin@mail.ru>
2023-04-19 08:32:36 +0300

    added main page

--- HermesVideoRental/Windows/MainWindow.xaml.cs
+++ HermesVideoRental/Windows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HermesVideoRental.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace HermesVideoRental
         public MainWindow()
         {
             InitializeComponent();
+            MainWindowFrame.Navigate(new MainPage());
         }
     }
 }
